64c16bcfe4872d594c7bb04b8b2e6d461ed80305
Ethan Davis <wetheethan@gmail.com>
2020-03-18 18:36:47 -0500

    Added spring data jpa dependency and created inventory repository

---
+++ src/main/java/com/tirgusapi/inventory/InventoryRepository.java
@@ -0,0 +1,7 @@
+package com.tirgusapi.inventory;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface InventoryRepository extends CrudRepository {
+}
---
+++ src/main/java/com/tirgusapi/user/User.java
@@ -0,0 +1,40 @@
+package com.tirgusapi.user;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private int id;
+   private String name;
+   private String email;
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+}
