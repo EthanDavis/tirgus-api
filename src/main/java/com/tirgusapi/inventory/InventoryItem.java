72c4f9cfef77bf55e12905785f0a7964f28e3431
Ethan Davis <wetheethan@gmail.com>
2020-03-28 23:53:24 -0500

    Updated inventory item object

--- src/main/java/com/tirgusapi/inventory/InventoryItem.java
+++ src/main/java/com/tirgusapi/inventory/InventoryItem.java
@@ -2,15 +2,40 @@ package com.tirgusapi.inventory;
 
 import org.joda.time.DateTime;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class InventoryItem {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private int id;
    private String name;
    private double price;
-   private DateTime createdAt;
+   private int quantity;
    private String description;
+   private DateTime createdAt;
 
    public InventoryItem(String name, double price) {
       this.name = name;
       this.price = price;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public int getQuantity() {
+      return quantity;
+   }
+
+   public void setQuantity(int quantity) {
+      this.quantity = quantity;
    }
 
    public String getName() {
