bbf7d8e77ee3078a67d41a047d95c32e4f07b2c2
Ethan Davis <wetheethan@gmail.com>
2020-03-29 00:01:01 -0500

    Started create item function in inventory controller

--- src/main/java/com/tirgusapi/inventory/InventoryController.java
+++ src/main/java/com/tirgusapi/inventory/InventoryController.java
@@ -1,11 +1,14 @@
 package com.tirgusapi.inventory;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InventoryController {
+
+
 
    @GetMapping("/items")
    @ResponseBody
@@ -13,4 +16,10 @@ public class InventoryController {
       return new InventoryItem("testItem", 12.50);
    }
 
+   @GetMapping("/items")
+   @ResponseBody
+   public InventoryItem createItem(@RequestBody InventoryItem item) {
+
+
+   }
 }
